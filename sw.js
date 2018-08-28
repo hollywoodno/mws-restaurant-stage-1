a01abe39ef1ef2a48b3aae914a4eb3425e143274
hollywoodno <hollywoodno@gmail.com>
2018-08-27 21:30:06 -0700

    clean up service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cache = 'mws-cache-1';
+const assetsCache = 'mws-cache-v1';
 
 const urlsToCache = [
     '/img/1.jpg',
@@ -10,50 +10,51 @@ const urlsToCache = [
     '/img/7.jpg',
     '/img/8.jpg',
     '/img/9.jpg',
-    '/img/10.jpg'
+    '/img/10.jpg',
+    '/css/styles.css',
+    '/js/restaurant_info.js',
+    '/js/main.js',
+    '/js/dbhelper.js',
+    'restaurant.html',
+    'index.html'
 ]
 
+/**
+ * Installs service worker with assets to cached
+ */
 self.addEventListener('install', evt => {
     console.log('installing....');
 
     evt.waitUntil(
-        // open a cache
-        caches.open(cache)
-        .then(cache => {
-            console.log('Cache is opened...');
-
-            // cache files
-            return cache.addAll(urlsToCache);
-        })
-        .catch(err => {
-            console.log('Cached failed: ', err);
-        })
+        caches.open(assetsCache)
+            .then(cache => {
+                console.log('Cache is opened...');
+                return cache.addAll(urlsToCache);
+            })
+            .catch(err => {
+                console.log('Cached install failed: ', err);
+            })
     )
 });
 
+/**
+ * Intercepts requests to serve/save cached assets
+ */
 self.addEventListener('fetch', evt => {
-    console.log('request coming through: ', evt.request.url);
 
     evt.respondWith(
-        // Check cache for image
-        caches.match(evt.request).then(rsp => {
-            if (rsp) {
-                console.log('serving a request from cached! request: ', evt.request.url)
-                
-                // We have intercepted the request here
-                // and are returning the response that was matched
-                // in the cache for this particular event.request
-                return rsp;
-            }
-
-            // at this point request not found in cache so
-            // send it on it's way as usual. 
-            // so we take the request and just send a response
-            // that is just re-making the exact same request
-            return fetch(evt.request)
+        // Open up cache for retrieval
+        // or caching of new asset
+        caches.open(assetsCache).then(cache => {
+            return caches.match(evt.request).then(rsp => {
+                return rsp || fetch(evt.request).then(rsp => {
+                    cache.put(evt.request, rsp.clone());
+                    return rsp;
+                });
+            })
+                .catch(err => {
+                    console.log('Unable to re-fetch request. Network error: ', err);
+                });
         })
-        .catch(err => {
-            console.log('Unable to re-fetch request: ', err);
-        })
-    )
+    );
 });
